3c18723771af655f80d9d3187eb1da7e204ca58d
Tatham Dees <john.t.dees@vanderbilt.edu>
2019-04-18 14:43:17 -0500

    adding ability to test multiple experiments

--- escape.py
+++ escape.py
@@ -3,16 +3,19 @@ import math
 
 #Runs the program
 def main():
-    numTrials = 1000
-    circleRadius = 15
+    runRadiusRange(1000 , 10)
+
+def runFullTrial(radius , num):
+    circleRadius = radius
+    numTrials = num
     trialList = []
     totalTrials = 0
     for i in range(0 , numTrials):
         trialList.append(runCalculation(circleRadius))
     for i in range (0 , numTrials):
-        print(trialList[i])
+        #print(trialList[i])
         totalTrials += trialList[i].numPoints
-    print()
+    #print()
     print("Total Trials: " + str(totalTrials))
     print ("Average Number of Trials: " + str(totalTrials / numTrials))
 
@@ -25,9 +28,11 @@ def calcCoordinates(x , dimensions):
         coordinates.append(y)
     return coordinates
 
+#Determines the point's distance from 0 in an x,y plane
 def getDistFromZero(point):
     return math.sqrt(math.pow(point[0] , 2) + math.pow(point[1] , 2))
 
+#returns 1 runthrough of the circle Escape algorithm
 def runCalculation(circleRadius):
     trials = 0
     currentCoordinates = [0] * 2
@@ -49,15 +54,30 @@ def runCalculation(circleRadius):
     outputTrial = trial(currentCoordinates ,trials )
     return outputTrial
 
+#runs an experiemnt for each radius within a range
+def runRadiusRange(numTrials , maxRadius):
+    for i in range(2 , maxRadius + 1):
+        print("Trials with radius: " + str(i) )
+        runFullTrial(i , numTrials)
+        print("")
+
+#class containing the final coordinates of a trial, as well as the number of points used to escape
 class trial():
     def __init__(self, coordinates , numPoints):
         self.coordinates = coordinates
         self.numPoints = numPoints
 
+    #formats the return
     def __str__(self):
         return str(str(self.coordinates) + " | " + str(self.numPoints))
 
+#TODO
+#Create an output of the average number of trials for 
 def makeCSV():
     print("j")
+
+#Notes/Todo
+#graph the number of points needed for each experiment (bar graph of #trials required for min to max number of trials in experiment)
+#Look into simple random walk. This is a 2D version
 
 main()
